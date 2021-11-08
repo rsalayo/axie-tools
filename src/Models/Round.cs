451f9be8fdc21ca8b58f7f4a89801d00834c486e
Romeo Salayo <rsalayo@openit.com>
2021-11-08 12:02:32 -0600

    Initial Commit

---
+++ src/Models/Round.cs
@@ -0,0 +1,17 @@
+﻿namespace Axie.Tools.Models
+{
+	public class Round
+	{
+		public int Id { get; set; }
+
+		public int Used { get; set; }
+
+		public int Gained { get; set; }
+
+		public int Destroyed { get; set; }
+
+		public int Remaining { get; set; }
+
+		public int Energy => this.Remaining + this.Gained - this.Used - this.Destroyed + 2;
+	}
+}
